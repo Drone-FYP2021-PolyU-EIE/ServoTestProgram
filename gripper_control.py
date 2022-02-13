65f74afa555205ab4734d83ec6ed992aee739ae5
Leung Machine <45313904+ctm6100@users.noreply.github.com>
2022-02-13 21:30:09 +0800

    Add files via upload

---
+++ gripper_control.py
@@ -0,0 +1,27 @@
+
+# Example of how to directly control a Pixhawk servo output with pymavlink.
+
+import time
+# Import mavutil
+from pymavlink import mavutil
+
+def set_servo_pwm(servo_n, microseconds):
+    # master.set_servo(servo_n+8, microseconds) or:
+    master.mav.command_long_send(
+        master.target_system, master.target_component,
+        mavutil.mavlink.MAV_CMD_DO_SET_SERVO,
+        0,            # first transmission of this command
+        servo_n + 8,  # servo instance, offset by 8 MAIN outputs
+        microseconds, # PWM pulse-width
+        0,0,0,0,0     # unused parameters
+    )
+
+# Create the connection
+master = mavutil.mavlink_connection('udpin:0.0.0.0:14550')
+# Wait a heartbeat before sending commands
+master.wait_heartbeat()
+
+# command servo_1 to go from min to max in steps of 50us, over 2 seconds
+for us in range(1100, 1900, 50):
+    set_servo_pwm(1, us)
+    time.sleep(0.125)
