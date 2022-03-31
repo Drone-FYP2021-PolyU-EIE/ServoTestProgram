e48dd09dd59514ec9ed03760ed260876f45f6b30
wingcrystal <yw.crystallo@gmail.com>
2022-03-30 19:24:22 -0700

    update

--- Grip_servo.py
+++ Grip_servo.py
@@ -1,11 +1,21 @@
-from sunau import AUDIO_FILE_ENCODING_LINEAR_16
 import time
-from adafruit_servokit import ServoKit
+import rospy
+# from adafruit_servokit import ServoKit
+from ros_circuitpython_servokit_msgs.msg import AllServoAngle
 
-kit = ServoKit(channels = 16)
-# ang = 180
-# kit.servo[0].angle = ang
+def gripper():
+    rospy.init_node('Gripper', anonymous=True)
+    servo_pub = rospy.Publisher('/servo/angle', AllServoAngle, queue_size=10)
+    angle = AllServoAngle()
+    angle.all16servoPWM = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    while not rospy.is_shutdown():
+        for i in range(0,180):
+            angle.all16servoPWM[0] = i
+            servo_pub.publish(angle)
+            time.sleep(0.05)
 
-for i in range(0,180):
-    kit.servo[0].angle = i
-    time.sleep(0.05)
+if __name__ == '__main__':
+	try:
+		gripper()
+	except rospy.ROSInterruptException:
+		pass
